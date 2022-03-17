c7f78a2b9697d7e336d63d6f97cca6e3294e905d
T•Ø•R•Ü•S <bageliq@protonmail.com>
2022-03-17 13:36:07 +0400

    Stopped the bot from popping words from the list Stopped it from removing the top words from the list unless they get disqualified.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,43 +8,49 @@
 	function click(e) {
 		let criteria = [];
 
-		let grays = [1, 2, 3, 4, 5].map((v) => document.getElementById(`gray-${v}`).value.toLowerCase());
+		let grays = [1, 2, 3, 4, 5].map((v) =>
+			document.getElementById(`gray-${v}`).value.toLowerCase()
+		);
 		// Get all yellow fields
-		let yellows = [1, 2, 3, 4, 5].map((v) => document.getElementById(`yellow-${v}`).value.toLowerCase());
+		let yellows = [1, 2, 3, 4, 5].map((v) =>
+			document.getElementById(`yellow-${v}`).value.toLowerCase()
+		);
 		// Get all green fields
-		let greens = [1, 2, 3, 4, 5].map((v) => document.getElementById(`green-${v}`).value.toLowerCase());
+		let greens = [1, 2, 3, 4, 5].map((v) =>
+			document.getElementById(`green-${v}`).value.toLowerCase()
+		);
 
 		grays.forEach((v, i) => {
-			if(v == '') {
+			if (v == '') {
 				return;
 			}
-			if(greens.includes(v)) {
+			if (greens.includes(v)) {
 				let occurences = 0;
-				yellows.forEach(c => v == c ? occurences++ : 0);
-				greens.forEach(c => v == c ? occurences++ : 0);
+				yellows.forEach((c) => (v == c ? occurences++ : 0));
+				greens.forEach((c) => (v == c ? occurences++ : 0));
 				criteria.push(new OnlyOccurance(v, occurences));
 				criteria.push(new NotAt(v, i));
 			}
-			if(yellows.includes(v) || greens.includes(v)) {
+			if (yellows.includes(v) || greens.includes(v)) {
 				let occurences = 0;
-				yellows.forEach(c => v == c ? occurences++ : 0);
-				greens.forEach(c => v == c ? occurences++ : 0);
+				yellows.forEach((c) => (v == c ? occurences++ : 0));
+				greens.forEach((c) => (v == c ? occurences++ : 0));
 				criteria.push(new OnlyOccurance(v, occurences));
 				criteria.push(new NotAt(v, i));
 			}
-			if(!greens.includes(v) && !yellows.includes(v)) {
+			if (!greens.includes(v) && !yellows.includes(v)) {
 				criteria.push(new No(v));
 			}
 		});
 
 		yellows.forEach((v, i) => {
 			if (v == '') {
-				return
+				return;
 			}
-			if(grays.includes(v) || greens.includes(v)) {
+			if (grays.includes(v) || greens.includes(v)) {
 				let occurences = 0;
-				yellows.forEach(c => v == c ? occurences++ : 0);
-				greens.forEach(c => v == c ? occurences++ : 0);
+				yellows.forEach((c) => (v == c ? occurences++ : 0));
+				greens.forEach((c) => (v == c ? occurences++ : 0));
 				criteria.push(new OnlyOccurance(v, occurences));
 			}
 			criteria.push(new Some(v, i));
@@ -54,25 +60,92 @@
 			if (v == '') {
 				return;
 			}
-			if(yellows.includes(v) || grays.includes(v)) {
+			if (yellows.includes(v) || grays.includes(v)) {
 				let occurences = 0;
-				yellows.forEach(c => v == c ? occurences++ : 0);
-				greens.forEach(c => v == c ? occurences++ : 0);
+				yellows.forEach((c) => (v == c ? occurences++ : 0));
+				greens.forEach((c) => (v == c ? occurences++ : 0));
 				criteria.push(new OnlyOccurance(v, occurences));
 			}
 			criteria.push(new Exact(v, i));
 		});
 
-		words = dict(
-			criteria,
-			words
-		)
+		words = dict(criteria, words);
 	}
 
 	function reset(_) {
 		words = dict([]);
 	}
 </script>
+
+<div class="flex justify-center items-center w-full flex-col">
+	<h1 class="text-2xl mx-auto">
+		<span class="text-gray-600">Gray</span>
+		letters:
+	</h1>
+
+	<div class="flex mx-auto w-4/5 translate-x-[25%]">
+		<input type="text" name="gray-1" id="gray-1" class="gray_box" maxlength="1" />
+		<input type="text" name="gray-2" id="gray-2" class="gray_box" maxlength="1" />
+		<input type="text" name="gray-3" id="gray-3" class="gray_box" maxlength="1" />
+		<input type="text" name="gray-4" id="gray-4" class="gray_box" maxlength="1" />
+		<input type="text" name="gray-5" id="gray-5" class="gray_box" maxlength="1" />
+	</div>
+
+	<br />
+
+	<h1 class="text-2xl mx-auto">
+		<span class="text-yellow-400">Yellow</span>
+		letters:
+	</h1>
+
+	<!-- Done like this because positional info is necessary -->
+	<div class="flex mx-auto w-4/5 translate-x-[25%]">
+		<input type="text" name="yellow-1" id="yellow-1" class="yellow_box" maxlength="1" />
+		<input type="text" name="yellow-2" id="yellow-2" class="yellow_box" maxlength="1" />
+		<input type="text" name="yellow-3" id="yellow-3" class="yellow_box" maxlength="1" />
+		<input type="text" name="yellow-4" id="yellow-4" class="yellow_box" maxlength="1" />
+		<input type="text" name="yellow-5" id="yellow-5" class="yellow_box" maxlength="1" />
+	</div>
+
+	<br />
+
+	<h1 class="text-2xl mx-auto">
+		<span class="text-green-400">Green</span>
+		letters:
+	</h1>
+
+	<div class="flex mx-auto w-4/5 translate-x-[25%]">
+		<input type="text" name="green-1" id="green-1" class="green_box" maxlength="1" />
+		<input type="text" name="green-2" id="green-2" class="green_box" maxlength="1" />
+		<input type="text" name="green-3" id="green-3" class="green_box" maxlength="1" />
+		<input type="text" name="green-4" id="green-4" class="green_box" maxlength="1" />
+		<input type="text" name="green-5" id="green-5" class="green_box" maxlength="1" />
+	</div>
+
+	<br />
+
+	<button
+		type="button"
+		on:click={click}
+		class="text-7xl bg-blue-300 rounded-xl p-3 m-3 mx-auto w-1/6 font-extrabold shadow-md"
+		>Try</button
+	>
+	<button
+		type="button"
+		on:click={reset}
+		class="text-7xl bg-blue-300 rounded-xl p-3 m-3 mx-auto w-1/6 font-extrabold shadow-md"
+		>Reset</button
+	>
+	<br />
+</div>
+
+<div class="flex flex-wrap">
+	<Top word={words[words.length - 1] ?? "Looks like we're stumped!"} />
+
+	{#each words as word}
+		<Guess {word} />
+	{/each}
+</div>
 
 <style lang="postcss">
 	.green_box {
@@ -87,161 +160,3 @@
 		@apply border-8 border-dashed border-gray-300 m-3 text-7xl w-[112px] font-extrabold shadow-md text-center uppercase;
 	}
 </style>
-
-<div class="flex justify-center items-center w-full flex-col">
-	<h1 class="text-2xl mx-auto">
-		<span class="text-gray-600">Gray</span>
-		letters:
-	</h1>
-
-	<div class="flex mx-auto w-4/5 translate-x-[25%]">
-		<input
-			type="text"
-			name="gray-1"
-			id="gray-1"
-			class="gray_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="gray-2"
-			id="gray-2"
-			class="gray_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="gray-3"
-			id="gray-3"
-			class="gray_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="gray-4"
-			id="gray-4"
-			class="gray_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="gray-5"
-			id="gray-5"
-			class="gray_box"
-			maxlength="1"
-		/>
-	</div>
-
-	<br />
-
-	<h1 class="text-2xl mx-auto">
-		<span class="text-yellow-400">Yellow</span>
-		letters:
-	</h1>
-
-	<!-- Done like this because positional info is necessary -->
-	<div class="flex mx-auto w-4/5 translate-x-[25%]">
-		<input
-			type="text"
-			name="yellow-1"
-			id="yellow-1"
-			class="yellow_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="yellow-2"
-			id="yellow-2"
-			class="yellow_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="yellow-3"
-			id="yellow-3"
-			class="yellow_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="yellow-4"
-			id="yellow-4"
-			class="yellow_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="yellow-5"
-			id="yellow-5"
-			class="yellow_box"
-			maxlength="1"
-		/>
-	</div>
-
-	<br />
-
-	<h1 class="text-2xl mx-auto">
-		<span class="text-green-400">Green</span>
-		letters:
-	</h1>
-
-	<div class="flex mx-auto w-4/5 translate-x-[25%]">
-		<input
-			type="text"
-			name="green-1"
-			id="green-1"
-			class="green_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="green-2"
-			id="green-2"
-			class="green_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="green-3"
-			id="green-3"
-			class="green_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="green-4"
-			id="green-4"
-			class="green_box"
-			maxlength="1"
-		/>
-		<input
-			type="text"
-			name="green-5"
-			id="green-5"
-			class="green_box"
-			maxlength="1"
-		/>
-	</div>
-
-	<br />
-
-	<button
-		type="button"
-		on:click={click}
-		class="text-7xl bg-blue-300 rounded-xl p-3 m-3 mx-auto w-1/6 font-extrabold shadow-md">Try</button
-	>
-	<button
-		type="button"
-		on:click={reset}
-		class="text-7xl bg-blue-300 rounded-xl p-3 m-3 mx-auto w-1/6 font-extrabold shadow-md">Reset</button
-	>
-	<br />
-</div>
-
-<div class="flex flex-wrap">
-	<Top word={words.shift() ?? "Looks like we're stumped!"} />
-
-	{#each words as word}
-		<Guess {word} />
-	{/each}
-</div>
